48eca4136c46689705aa9cf6f3eea30b919bdee4
shubham verma <shubham.verma@variablesoft.com>
2026-08-15 10:50:31 +0530

    build: bump version code to 23

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         applicationId = "com.dotfield.dotcal"
         minSdk = 30
         targetSdk = 36
-        versionCode = 22
+        versionCode = 23
         versionName = "1.3.0"
     }
 
